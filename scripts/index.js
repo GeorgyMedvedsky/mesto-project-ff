95d92dc651b20c9f2d876463f97ffd19ae696da7
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-02-28 13:15:53 +0300

    feat: add render cards / add function delete card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+// DOM узлы
+const cardsContainer = document.querySelector('.places__list');
+// функция создания карточки
+function createCard(cardData, callback) {
+    // создать клон карточки
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: DOM узлы
+    // найти элементы созданной карточки
+    const cardImage = newCard.querySelector('.card__image');
+    const cardName = newCard.querySelector('.card__title');
+    const deleteCardBtn = newCard.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+    // наполнить новую карточку содержимым
+    cardImage.src = cardData.link;
+    cardName.textContent = cardData.name;
+    deleteCardBtn.addEventListener('click', callback);
+    
+    // вернуть готовую карточку
+    return newCard;
+}
+// функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// вывод карточек на страницу
+initialCards.forEach(card => {
+    const newCard = createCard(card, deleteCard);
+    cardsContainer.append(newCard);
+});
